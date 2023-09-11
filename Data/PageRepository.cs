c4a4fcf8767a1516cebc42db2b9becaa1d061a63
Abhinav <abhinavborde.mail@gmail.com>
2023-09-11 11:55:29 +0530

    test1

--- Data/PageRepository.cs
+++ Data/PageRepository.cs
@@ -28,5 +28,31 @@
             await _context.SaveChangesAsync();
             return true;
         }
+
+        public async Task<IActionResult> UpdatePage(Page page, int id)
+        {
+            //throw new NotImplementedException();
+            bool isDuplicateTitle = await _context.Website.AnyAsync(a => a.URL == page.url && a.id != page.id);
+
+            if (isDuplicateTitle)
+            {
+                throw new InvalidOperationException("A page with the same URL already exists.");
+            }
+
+            _context.Entry(page).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return null;
+        }
+
+        public async Task<bool> DeletePage(int id)
+        {
+            var pageToDelete = await _context.Page.FindAsync(id);
+            if (pageToDelete == null)
+                return false;
+
+            _context.Page.Remove(pageToDelete);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
